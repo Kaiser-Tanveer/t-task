8bfd84bc7a014518d28901796cbe4934d48b0e23
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-02-09 11:21:33 +0600

    router setup ipmlemented

---
+++ src/Routes/Routes.js
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Shared/Layouts/Main";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+])
+
+export default router;
